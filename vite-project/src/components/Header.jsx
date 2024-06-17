735f07e07cd0c26e0d65e34c96bfabdb6e97c9e0
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-17 09:37:45 +0200

    Set up the navbar in my `Header` component with favourites and search

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import imageUrl from "/src/assets/images/avatar-icon.png";
 
-const Header = () => {
+export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <header>
-      <h1>Podcast App</h1>
+      <Link className="site-logo" to="/">
+        Sound Silo
+      </Link>
+      <nav>
+        <NavLink
+          to="/favourites"
+          className={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Favourites
+        </NavLink>
+        <NavLink
+          to="/search"
+          className={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Search
+        </NavLink>
+        <Link to="login" className="login-link">
+          <img src={imageUrl} className="login-icon" />
+        </Link>
+        <button onClick={fakeLogOut}>X</button>
+      </nav>
     </header>
   );
-};
-
-export default Header;
+}

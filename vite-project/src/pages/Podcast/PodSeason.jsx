479a695c1f28c28876168db002f597014bc901cd
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-18 10:32:34 +0200

    Took the `Season` object as a prop and rendered a page for a specific season of a show

--- vite-project/src/pages/Podcast/PodSeason.jsx
+++ vite-project/src/pages/Podcast/PodSeason.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 import Episode from "./PodEpisode";
 
-const Season = ({ season }) => (
-  <div>
-    <h3>Season {season.seasonNumber}</h3>
-    {season.episodes.map((episode) => (
-      <Episode key={episode.episodeNumber} episode={episode} />
-    ))}
-  </div>
-);
+const Season = ({ season }) => {
+  const { showId } = useParams();
+
+  return (
+    <div>
+      <h3>Season {season.seasonNumber}</h3>
+      <Link to={`/show/${showId}`}>Back to Show</Link>
+      {season.episodes.map((episode) => (
+        <Episode key={episode.episodeNumber} episode={episode} />
+      ))}
+    </div>
+  );
+};
 
 export default Season;

dccb815f8bc5d4969f62e4a89a3de40735d59775
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-19 15:17:58 +0200

    Sorted the options for the `ShowList` function

--- vite-project/src/pages/Podcast/ShowList.jsx
+++ vite-project/src/pages/Podcast/ShowList.jsx
@@ -1,27 +1,41 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { formatDistanceToNow } from "date-fns";
 
-const Showlist = ({ shows }) => {
-  <div>
-    <h2>All Shows</h2>
-    <ul>
-      {shows.map((show) => (
-        <li key={show.id}>
-          <Link to={`/show/${show.id}`}>
-            <img src={show.image} alt={`${show.title} poster`} width="100" />
-            <div>{show.title}</div>
-            <div>Genres: {show.genres.join(", ")}</div>
-            <div>Seasons: {show.seasons.length}</div>
-            <div>
-              Last Updated: {formatDistanceToNow(new Date(show.lastUpdated))}
-              ago
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>;
-};
+export default function ShowList(shows) {
+  const [sortOption, setSortOption] = useState("title_az");
 
-export default Showlist;
+  const sortShows = (shows) => {
+    if (sortOption === "title_az") {
+      return shows.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortOption === "title_za") {
+      return shows.sort((a, b) => b.title.localeCompare(a.title));
+    }
+    return shows;
+  };
+
+  const sortedShows = sortShows([...shows]);
+
+  return (
+    <div>
+      <h2>All Shows</h2>
+      <div>
+        <label>Sort by: </label>
+        <select
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="title_az">Title A-Z</option>
+          <option value="title_za">Title Z-A</option>
+        </select>
+      </div>
+      <ul>
+        {sortedShows.map((show) => (
+          <li key={show.id}>{show.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

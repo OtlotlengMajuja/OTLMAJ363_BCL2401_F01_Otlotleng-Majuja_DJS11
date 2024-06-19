9c93c80e13466a31573549cf64343d1c43ccb4a3
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-19 19:38:49 +0200

    Added a button element that calls the `handlePlay` function to play the episode

--- vite-project/src/pages/Podcast/PodEpisode.jsx
+++ vite-project/src/pages/Podcast/PodEpisode.jsx
@@ -1,9 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useFavorites } from "../../context/FavesContext";
+import { useAudioPlayer } from "../../context/AudioPlayerContext";
 
 export default function PodEpisode(episode, showTitle, seasonNumber) {
   const { faves, addFavorite, removeFavorite } = useFavorites();
+  const { playEpisode } = useAudioPlayer();
+
   const isFavorite = faves.some((fav) => fav.id === episode.id);
 
   const toggleFavorite = () => {
@@ -14,13 +17,14 @@ export default function PodEpisode(episode, showTitle, seasonNumber) {
     }
   };
 
+  const handlePlay = () => {
+    playEpisode(episode);
+  };
+
   return (
     <div>
       <h4>{episode.title}</h4>
-      <audio controls>
-        <source src={episode.audioUrl} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <button onClick={handlePlay}>Play</button>
       <button onClick={toggleFavorite}>
         {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
       </button>

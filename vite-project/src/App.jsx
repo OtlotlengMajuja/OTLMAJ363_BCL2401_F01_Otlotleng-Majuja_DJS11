da0f4d143bba4d928479ed7eaa595a1676742b19
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-19 19:27:38 +0200

    Updated the `App` to include the `AudioPlayer` route

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,7 +7,9 @@ import ShowList from "./pages/Podcast/ShowList";
 import PodDetails from "./pages/Podcast/PodDetails";
 import PodSeason from "./pages/Podcast/PodSeason";
 import GroupedFaves from "./components/GroupedFaves";
+import AudioPlayer from "./components/AudioPlayer";
 import { FavoritesProvider } from "./context/FavesContext";
+import { AudioPlayerProvider } from "./context/AudioPlayerContext";
 
 export default function App() {
   const [shows, setShows] = useState([]);
@@ -23,18 +25,21 @@ export default function App() {
 
   return (
     <FavoritesProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<ShowList shows={shows} />} />
-          <Route path="/show/:showId" element={<PodDetails />} />
-          <Route
-            path="/show/:showId/season/:seasonNumber"
-            element={<PodSeason />}
-          />
-          <Route path="/favorites" element={<GroupedFaves />} />
-        </Routes>
-      </BrowserRouter>
+      <AudioPlayerProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<ShowList shows={shows} />} />
+            <Route path="/show/:showId" element={<PodDetails />} />
+            <Route
+              path="/show/:showId/season/:seasonNumber"
+              element={<PodSeason />}
+            />
+            <Route path="/favorites" element={<GroupedFaves />} />
+          </Routes>
+          <AudioPlayer />
+        </BrowserRouter>
+      </AudioPlayerProvider>
     </FavoritesProvider>
   );
 }

8a4d7a43e9c7ebcccd665d1f71b4b871e1df2814
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-20 10:11:36 +0200

    Included the `handleEnded` function to track and save listening progress

--- vite-project/src/components/AudioPlayer.jsx
+++ vite-project/src/components/AudioPlayer.jsx
@@ -9,21 +9,37 @@ export default function AudioPlayer(episode) {
   useEffect(() => {
     const audio = audioRef.current;
     const updateTime = () => setCurrentTime(audio.currentTime);
+
     const handleBeforeUnload = (e) => {
       if (audio && !audio.paused) {
         e.preventDefault();
         e.returnValue = "";
       }
     };
+
+    const handleEnded = () => {
+      let completedEpisodes =
+        JSON.parse(localStorage.getItem("completedEpisodes")) || [];
+      if (!completedEpisodes.includes(episode.id)) {
+        completedEpisodes.push(episode.id);
+        localStorage.setItem(
+          "completedEpisodes",
+          JSON.stringify(completedEpisodes)
+        );
+      }
+    };
+
     if (audio) {
       audio.addEventListener("timeupdate", updateTime);
+      audio.addEventListener("ended", handleEnded);
       window.addEventListener("beforeunload", handleBeforeUnload);
       return () => {
         audio.removeEventListener("timeupdate", updateTime);
+        audio.removeEventListener("ended", handleEnded);
         window.removeEventListener("beforeunload", handleBeforeUnload);
       };
     }
-  }, [audioRef]);
+  }, [episode]);
 
   if (!episode) return null;
 

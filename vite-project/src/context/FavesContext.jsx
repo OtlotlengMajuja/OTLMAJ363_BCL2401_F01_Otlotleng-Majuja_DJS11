814a448dac975ef76d16ac6cf873c2e1986e6a47
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-20 10:01:02 +0200

    Included the `storedFaves` function  that saves and loads favorites from localStorage, and `clearFaves` to clear the favorites field

--- vite-project/src/context/FavesContext.jsx
+++ vite-project/src/context/FavesContext.jsx
@@ -1,31 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { createContext, useContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FavesContext = createContext();
 
 export const useFavorites = () => useContext(FavesContext);
 
 export default function FavoritesProvider(children) {
-  const [faves, setFaves] = useState([]);
+  const [faves, setFaves] = useState(() => {
+    const storedFaves = localStorage.getItem("favorites");
+    return storedFaves ? JSON.parse(storedFaves) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(faves));
+  }, [faves]);
 
   const addFavorite = (episode, showTitle, seasonNumber) => {
-    const newFavorite = {
-      ...episode,
-      showTitle,
-      seasonNumber,
-      addedAt: new Date().toISOString(), // Add timestamp
-    };
-    setFaves((prevFaves) => [...prevFaves, newFavorite]);
+    setFaves([
+      ...faves,
+      { ...episode, showTitle, seasonNumber, addedAt: new Date() },
+    ]);
   };
 
   const removeFavorite = (episodeId) => {
-    setFaves((prevFaves) => prevFaves.filter((ep) => ep.id !== episodeId));
+    setFaves(faves.filter((fav) => fav.id !== episodeId));
+  };
+
+  const clearFaves = () => {
+    setFaves([]);
   };
 
   return (
-    <FavesContext.Provider value={{ faves, addFavorite, removeFavorite }}>
+    <FavesContext.Provider
+      value={{ faves, addFavorite, removeFavorite, clearFaves }}
+    >
       {children}
     </FavesContext.Provider>
   );

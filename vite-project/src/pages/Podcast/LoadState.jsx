a71af7fe599e2c8bf192092248b71ad8108a94dd
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-20 19:15:20 +0200

    Added a timeout feature to `LoadState` that displays a message if the loading takes too long

--- vite-project/src/pages/Podcast/LoadState.jsx
+++ vite-project/src/pages/Podcast/LoadState.jsx
@@ -1,8 +1,38 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function LoadState() {
-  <div className="loader">
-    <p>Loading...</p>
-  </div>;
+export default function LoadState({
+  loading,
+  error,
+  children,
+  timeout = 3000,
+}) {
+  const [timeoutReached, setTimeoutReached] = useState(false);
+
+  if (loading) {
+    setTimeout(() => {
+      setTimeoutReached(true);
+    }, timeout);
+
+    return timeoutReached ? (
+      <div>Loading took too long...</div>
+    ) : (
+      <div>Loading...</div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return children;
 }
+
+LoadState.propTypes = {
+  loading: PropTypes.node.isRequired,
+  error: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+  timeout: PropTypes.node.isRequired,
+};
